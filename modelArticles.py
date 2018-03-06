e92274296a7b7bcd43acba213b6657c76ce867f5
Michelle McSweeney <michelleamcsweeney@gmail.com>
2018-03-06 17:31:01 -0500

    modeling

--- modelArticles.py
+++ modelArticles.py
@@ -9,10 +9,14 @@ Created on Mon Mar  5 16:41:58 2018
 import nltk
 import pandas as pd
 import numpy as np
-import shelve
+
 
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.cluster import KMeans
 
 
 def makedf(paper):
@@ -21,31 +25,56 @@ def makedf(paper):
     for word in badwords:
         df = df[df.content.str.contains(word) ==False]
     return df
-                
-#wordnet_lemmatizer = WordNetLemmatizer()
-#
-#def my_lemmatizer(s):
-#    s =s.lower()
-#    tokens = nltk.tokenize.word_tokenize(s)
-#    tokens = [t for t in tokens if len(t) >2]
-#    tokens = [wordnet_lemmatizer.lemmatize(t) for t in tokens]
-#    tokens = [t for t in tokens if t not in stopwords.words('english')]
-#    return tokens
-#
-#word_index_map = {}
-#current_index = 0
-#tokenized=[]
-#
-#for row in df:
-#    tokens = my_tokenizer(df[])
-#
-#def article_compiler(art):
-#    pass
-    
+
+stops = stopwords.words('english')
+wordnet_lemmatizer = WordNetLemmatizer()
+
+def my_tokenizer(s):
+    s =s.lower()
+    tokens = nltk.tokenize.word_tokenize(s)
+    tokens = [t for t in tokens if len(t) >2]
+    tokens = [wordnet_lemmatizer.lemmatize(t) for t in tokens]
+    tokens = [t for t in tokens if t not in stops]
+    return tokens
+
+
+mydf = makedf('msnbc')
+
+
+tokenized=[]
+for k,v in mydf.iterrows():
+    tok = (v['content'])
+    tokens = my_tokenizer(tok)
+    for t in tokens:
+        tokenized.append(t)
+
+print(len(tokenized))
+
+tfidf_vectorizer = TfidfVectorizer(max_df = 0.3, min_df = 20, stop_words=stops)
+tfidf = tfidf_vectorizer.fit_transform(tokenized)
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+nmf = NMF(n_components = 10, random_state = 1, alpha = .1).fit(tfidf)
+
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print()
+
+
+transformed_data = nmf.transform(tfidf)
+len(transformed_data)
+
+print_top_words(nmf, tfidf_feature_names, 20)
+
         
 
-if __name__ == "__main__":
-    mydf = makedf('breitbart')
-    for k,v in mydf.iterrows():
-        print(v['content'])
+
+    
+        
+#
+#if __name__ == "__main__":
+#    mydf = makedf('breitbart')
+
     
